b7402eb89c2427d3d7225c3db643eefc5e30ac17
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-21 17:15:18 +0300

    Import user-service and add function

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,23 +1,60 @@
+import User from "../entity/User";
+import userService from "../service/user-service";
+
+// TODO Сделать типизацию req, res
+/* interface reqType {
+  firstName: string,
+  lastName: string,
+  email: string, 
+  password: string,
+  dateBirth: string,
+}
+ */
+
 class UserController {
-
   async createUser(req, res) {
-
+    try {
+      const newUser = await userService.createUser(req.body);
+      res.json(newUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 
   async getUsers(req, res) {
-
+    try {
+      const users = await userService.getUsers();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 
   async getUser(req, res) {
-
+    try {
+      const user = await userService.getUser(req.params.id);
+      return res.json(user);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
   }
 
   async updateUser(req, res) {
-
+    try {
+      const updatedUser = await userService.updateUser(req.body);
+      return res.json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
   }
 
   async deleteUser(req, res) {
-
+    try {
+      const deletedUser = await userService.deleteUser(req.params.id);
+      return res.json(deletedUser);
+    } catch (err) {
+      res.status(500).json(err.message);
+    }
   }
 }
 
