2913f07de52f47af2cec4d52909f16ee93a5d4a0
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-21 12:57:22 +0300

    Create user controller

---
+++ src/controller/user-controller.ts
@@ -0,0 +1,24 @@
+class UserController {
+
+  async createUser(req, res) {
+
+  }
+
+  async getUsers(req, res) {
+
+  }
+
+  async getUser(req, res) {
+
+  }
+
+  async updateUser(req, res) {
+
+  }
+
+  async deleteUser(req, res) {
+
+  }
+}
+
+export default new UserController();
