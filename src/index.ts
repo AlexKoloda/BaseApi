52f08d7541a5da6ca977e11fe2232be29e71888d
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-21 11:42:17 +0300

    Add migrations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { AppDataSource } from "./db/dataSource";
+import * as express from "express";
+/* import allRoutes from "./routes/routes"; */
 
-AppDataSource.initialize().then(async () => {
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(allRoutes);
+
+AppDataSource.initialize()
+
+  .then(async () => {
+    console.log("Connected to db");
+  })
+
+  .catch((error) => console.log(error));
+
+app.listen(3000, function () {
+  console.log("Сервер ожидает подключения...");
+});
+
+
+
+
+
+
+// TODO Автоматический шаблон, удалить после того, как разберусь с созданием Юзера.
+/* AppDataSource.initialize().then(async () => {
 
     console.log("Inserting a new user into the database...")
     const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
+    user.firstName = "Alex"
+    user.lastName = "Koloda"
+    user.email = "ex@mail.com"
+    user.password = "password"
+    user.dateBirth = "11-11-2011"
+
     await AppDataSource.manager.save(user)
     console.log("Saved a new user with id: " + user.id)
 
@@ -17,4 +44,4 @@ AppDataSource.initialize().then(async () => {
 
     console.log("Here you can setup and run express / fastify / any other framework.")
 
-}).catch(error => console.log(error))
+}).catch(error => console.log(error)) */
