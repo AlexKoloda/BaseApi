10aebcbd6eae4c0b79580d872e2f51ea9d28de1c
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-21 17:18:29 +0300

    Add routes

---
+++ src/routes/user-routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import UserController from "../controller/user-controller";
+
+const router = Router()
+
+
+router.post('/user', UserController.createUser);
+router.get('/user', UserController.getUsers);
+router.get('/user/:id', UserController.getUser);
+router.put('/user', UserController.updateUser);
+router.delete('/user/:id', UserController.deleteUser);
+
+export default router;
