1a1720d51c6965d8bb5b8f30cdc807eb27b8e49c
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-22 15:31:52 +0300

    Change router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { AppDataSource } from "./db/data-source";
 import * as express from "express";
-import userRouter from "./User/routes/user-routes";
+import mainRouter from "./routes";
 
 const app = express();
 const PORT = 3000;
@@ -11,11 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 AppDataSource.initialize()
 
   .then(async () => {
-    app.use("/api", userRouter);
     console.log("Connected to db");
   })
 
   .catch((error) => console.log(error));
+
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Сервер ожидает подключения на порту ${PORT}`);
