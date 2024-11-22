16aa34b4742f7f77fd98a0150e91c3ba328d9443
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-22 12:37:14 +0300

    Add user repsitory and type of function

--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,42 +1,39 @@
-import { User } from "../entity/User";
+import { userRepository } from "../repository/user-repository";
+import { CreateUserInterface, GetUserInterface } from "../types/types";
 
 class UserService {
-
-  async createUser(user) {
-      const newUser = await User.create(user);
-      return newUser;
+  async createUser(user: CreateUserInterface) {
+    const newUser = await userRepository.create(user);
+    return newUser;
   }
 
-  async getUsers() {   
-      const users = await User.find();
-      return users;  
+  async getUsers(): Promise<GetUserInterface[]> {
+    const users = await userRepository.find();
+    return users;
   }
 
-  async getUser(userId) {
-
-      if(!userId) {
-        throw new Error("Не указан id");
-      }
-      const user = await User.findOneBy({id:userId});
-      return user;
-  }
-
-  async updateUser(user) {
-
-    if(!user._id) {
-      throw new Error("Не указан id"); 
+  async getUser(userId: number) {
+    if (!userId) {
+      throw new Error("Не указан id");
     }
-    const updatedUser = await User.findOneByUpdate(user._id, user, {new: true}) //TODO Придумать реализацию редактирования 
+    const user = await userRepository.findOneBy({ id: userId });
+    return user;
+  }
+
+  async updateUser(user: GetUserInterface) {
+    if (!user.id) {
+      throw new Error("Не указан id");
+    }
+    const updatedUser = await userRepository.update(user.id, user); //TODO Проверить реализацию редактирования
     return updatedUser;
   }
 
-  async deleteUser(userId) {   
-
-      if(!userId) {
-        throw new Error("Не указан id");
-      }
-      const user = await User.findByIdAndDelete(userId) //TODO Придумать реализацию удаления 
-      return user;   
+  async deleteUser(userId: number) {
+    if (!userId) {
+      throw new Error("Не указан id");
+    }
+    const user = await userRepository.delete(userId);
+    return user;
   }
 }
 
