39ef5a16483283d4f0cecb26588b557ef3495d58
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-22 16:56:06 +0300

    Add initialize

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,17 +1,24 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "postgres",
-    synchronize: false,
-    logging: false,
-    entities: [`${__dirname}/../entity/*.{ts,js}`],
-    migrations: [`${__dirname}/migrations/*.{ts,js}`],
-    subscribers: [],
-})
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "postgres",
+  synchronize: false,
+  logging: false,
+  entities: [`${__dirname}/../**/entity/*.ts`],
+  migrations: [`${__dirname}/migrations/*.{ts,js}`],
+  subscribers: [],
+});
+
+AppDataSource.initialize()
+
+  .then(async () => {
+    console.log("Connected to db");
+  })
+
+  .catch((error) => console.log(error));
