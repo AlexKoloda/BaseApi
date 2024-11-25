a2d5b9cc57ac199b6523bb16ccd8097c35965ae4
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-25 09:50:59 +0300

    Add  middleware for authenticate

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth-routes";
 import userRouter from "./user-routes";
+import passport = require("passport");
 
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRouter);
-mainRouter.use("/user", userRouter);
+mainRouter.use("/user", passport.authenticate("jwt", {session: false}), userRouter);
 
 export default mainRouter;
