c24eef65fb10e72f0544c36e9e31bdb6b7de5044
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-25 14:40:09 +0300

    Fix authenticate

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth-routes";
 import userRouter from "./user-routes";
-import passport = require("passport");
+import authenticateToken from "../middleware";
 
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRouter);
-mainRouter.use("/user", passport.authenticate("jwt", {session: false}), userRouter);
+mainRouter.use("/user", authenticateToken, userRouter);
 
 export default mainRouter;
