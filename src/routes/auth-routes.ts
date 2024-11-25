66f0e492feb362253d6c903e5b22d23400e108eb
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-25 14:45:17 +0300

    Add sign-in route

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -4,5 +4,6 @@ import authController from "../Authentication/controller/auth-controller";
 const authRouter = Router();
 
 authRouter.post("/sign-up", authController.registration);
+authRouter.post("/sign-in", authController.login);
 
 export default authRouter;
