86e4569b7cf17ff626f23c7a3c11041bed1c3ad0
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-26 10:01:58 +0300

    Change class to arrow function

--- src/Authentication/controller/auth-controller.ts
+++ src/Authentication/controller/auth-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import authService from "../service/auth-service";
-import token from "../../util/token";
+import createJwt from "../../util/token";
 import { isValid } from "../../validation/yups";
 
 class AuthController {
@@ -11,7 +11,7 @@ class AuthController {
       }
 
       const newUser = await authService.registration(req.body);
-      const currentToken = token.createJwt(newUser.email);
+      const currentToken = createJwt(newUser.email);
       res.status(201).setHeader("authorization", "Bearer" + currentToken).json(newUser);
     } catch (err) {
       res.status(500).json(err.message);
@@ -22,7 +22,7 @@ class AuthController {
     try {
       const { email, password } = req.body;
       const user = await authService.login(email, password);
-      const currentToken = token.createJwt(email);
+      const currentToken = createJwt(email);
       res.status(200).json({user: user, token: currentToken});
     } catch (err) {
       res.status(500).json(err);
--- src/util/token.ts
+++ src/util/token.ts
@@ -2,8 +2,8 @@ import { sign } from "jsonwebtoken";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-class Token {
-  createJwt(email: string) {
+
+const createJwt = (email: string) => {
     try {
       const token = sign({ email }, process.env.TOKEN_SECRET, {
         expiresIn: "24h",
@@ -13,6 +13,6 @@ class Token {
       console.log(err);
     }
   }
-}
 
-export default new Token();
+
+export default createJwt;
