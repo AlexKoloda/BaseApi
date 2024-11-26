a1bc898ca6086049493c183462d192f61decee5a
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-26 19:33:18 +0300

    Add global error cath

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import * as express from "express";
 import mainRouter from "./routes";
-import * as dotenv from "dotenv";
-dotenv.config();
+import { config } from "dotenv";
+import { CustomError, NotValidDataError }  from "./util/custom-errors";
+config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));      
 app.use("/", mainRouter);
+
+app.use((err:Error, req, res, next) => {
+
+  if (err instanceof CustomError) {
+    return res.status(err.status).json({message: err.message, data: err.payload})
+  }
+
+  res.status(500).json({message: 'Server internal error'})
+})
 
 app.listen(PORT, () => {
   console.log(`Сервер ожидает подключения на порту ${PORT}`);
