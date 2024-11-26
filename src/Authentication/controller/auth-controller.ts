84bbc230bb35fd07907b9bb7d20fd34a93957cab
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-26 11:03:12 +0300

    Fix validate

--- src/Authentication/controller/auth-controller.ts
+++ src/Authentication/controller/auth-controller.ts
@@ -6,13 +6,18 @@ import { isValid } from "../../validation/yups";
 class AuthController {
   async registration(req: Request, res: Response) {
     try {
-      if (isValid) {
-        throw new Error('Данные не верны')
+      const valid = await isValid(req.body);
+
+      if (!valid) {
+        throw new Error("Не верные данные");
       }
 
       const newUser = await authService.registration(req.body);
       const currentToken = createJwt(newUser.email);
-      res.status(201).setHeader("authorization", "Bearer" + currentToken).json(newUser);
+      res
+        .status(201)
+        .setHeader("authorization", "Bearer" + currentToken)
+        .json(newUser);
     } catch (err) {
       res.status(500).json(err.message);
     }
@@ -23,7 +28,7 @@ class AuthController {
       const { email, password } = req.body;
       const user = await authService.login(email, password);
       const currentToken = createJwt(email);
-      res.status(200).json({user: user, token: currentToken});
+      res.status(200).json({ user: user, token: currentToken });
     } catch (err) {
       res.status(500).json(err);
       console.log(err);
