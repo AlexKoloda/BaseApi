c3b22c8fabb7c9cac71d1ddec6d13bae3282508d
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-26 11:20:48 +0300

    Feat env

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,13 +1,16 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { config } from "dotenv";
+config();
+
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
+  host: process.env.HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
   entities: [`${__dirname}/../**/entity/*.ts`],
