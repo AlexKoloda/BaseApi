7e953648785cfd6d7c920ea3175faf1aa44a9f00
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-26 09:53:36 +0300

    Feat validation for user data

--- src/validation/yups.ts
+++ src/validation/yups.ts
@@ -1,10 +1,14 @@
-/* import * as  yup from 'yup';
+import { string, object } from "yup";
+import { CreateUserInterface } from "../types/types";
 
-const userSchema = yup.object({
-  firstName: yup.string().min(1),
-  lastName: yup.string().min(1),
-  email: yup.string.email(),
-  password: yup.string().min(6),
-  dateBirth: yup.string().min(1),
+const userSchema = object({
+  firstName: string().min(1).required(),
+  lastName: string().min(1).required(),
+  email: string().email().required(),
+  password: string().min(6).required(),
+  dateBirth: string().min(1).required(),
+});
 
-}) */
+export const isValid = async (data: CreateUserInterface) => {
+  return await userSchema.isValid(data);
+};
