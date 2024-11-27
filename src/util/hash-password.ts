e3193b1e5347d54ef193086fdb677ffaa6cf5903
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-27 19:29:23 +0300

    Rename func

--- src/util/hash-password.ts
+++ src/util/hash-password.ts
@@ -2,7 +2,7 @@ import { pbkdf2Sync } from "crypto";
 import { config } from "dotenv";
 config();
 
-export const generatePassword = (password: string) => {
+export const generateHashPassword = (password: string) => {
   const salt = process.env.SALT;
   const generateHash = pbkdf2Sync(password, salt, 1000, 64, "sha512").toString(
     "hex"
