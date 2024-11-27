d73ba4f2e1d0af22f5628c0131f38f40ae7a82fd
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-27 19:31:20 +0300

    Refactor

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth-routes";
 import userRouter from "./user-routes";
-import authenticateToken from "../middleware";
+import { authenticateToken } from "../middleware";
 
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRouter);
 mainRouter.use("/user", authenticateToken, userRouter);
 export default mainRouter;
+ 
