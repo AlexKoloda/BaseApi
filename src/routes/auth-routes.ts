a9efa8067124cfb42636ca6a1b735744cb380183
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-27 19:26:09 +0300

    Add validate

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authController from "../ controllers/auth-controller";
+import { } from "../middleware";
+import { signInSchema, userSchema, validate } from "../validation/yups";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", authController.registration);
-authRouter.post("/sign-in", authController.login);
+authRouter.post("/sign-up", validate(userSchema), authController.registration);
+authRouter.post("/sign-in", validate(signInSchema), authController.login);
 
 export default authRouter;
