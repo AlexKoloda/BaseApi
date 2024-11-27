98420a94430a7e68b07c34ae134f24093a708fd8
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-27 19:30:40 +0300

    Add validation

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import userController from "../ controllers/user-controller";
+import userController from "../controllers/user-controller";
+import { idSchema, userSchema, validate, validateId } from "../validation/yups";
 
 const userRouter = Router();
 
 userRouter.get("/all", userController.getUsers);
-userRouter.get("/:id", userController.getUser);
-userRouter.patch("/", userController.updateUser);
-userRouter.delete("/:id", userController.deleteUser);
+userRouter.get("/:id", validateId(idSchema), userController.getUser);
+userRouter.patch("/", validate(userSchema), userController.updateUser);
+userRouter.delete("/:id", validate(idSchema), userController.deleteUser);
 
 export default userRouter;
