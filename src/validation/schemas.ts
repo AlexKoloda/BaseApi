540ba0d8c65b0a1d1ad69d67fa8aa7956db94336
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-28 15:26:14 +0300

    Change path and refactor

---
+++ src/validation/schemas.ts
@@ -0,0 +1,48 @@
+import { string, object } from "yup";
+
+export const createUserSchema = object({
+  firstName: string()
+    .min(1, "Name must be more than 1 characters")
+    .required("Name must be require"),
+  lastName: string()
+    .min(1, "Surname must be more than 1 characters")
+    .required("Surname must be require"),
+  email: string()
+    .email("Email must be with ex@ex.com")
+    .required("Email must be require"),
+  password: string()
+    .min(6, "Password must be more than 6 characters")
+    .required("Password must be require"),
+  dateBirth: string().min(1).required("Field must be require"),
+});
+
+export const signInSchema = object({
+  email: string()
+    .email("Email must be with ex@ex.com")
+    .required("Email must be require"),
+  password: string()
+    .min(6, "Password must be more than 6 characters")
+    .required("Password must be require"),
+});
+
+export const idSchema = object({
+  id: string()
+    .min(1, "Id must be more than 1 characters")
+    .required("Id must be require"),
+});
+
+export const updateUserSchema = object({
+  id: string()
+    .min(1, "Id must be more than 1 characters")
+    .required("Id must be require"),
+  firstName: string()
+    .min(1, "Name must be more than 1 characters")
+    .required("Name must be require"),
+  lastName: string()
+    .min(1, "Surname must be more than 1 characters")
+    .required("Surname must be require"),
+  email: string()
+    .email("Email must be with ex@ex.com")
+    .required("Email must be require"),
+  dateBirth: string().min(1).required("Field must be require"),
+});
