02d7ee884b3f1e2085d3839a40b56ffc92bec689
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-28 15:29:01 +0300

    Change path

---
+++ src/validation/validate.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { ValidationError } from "yup";
+import { ValidationFailure } from "../util/custom-errors";
+import { parseValidationErrors } from "../util/errorReader";
+import { UserSchema } from "../types/types";
+
+export const validate = (schema: UserSchema) => async (req: Request, res:Response, next:NextFunction) => {
+  try {
+    await schema.validate(req.body, { abortEarly: false });
+    next();
+  } catch (err) {
+    if (err instanceof ValidationError) {
+        const customError = parseValidationErrors(err)
+        next(new ValidationFailure(customError))
+        return
+      }
+      next(err) 
+  }
+};
+
+
+
