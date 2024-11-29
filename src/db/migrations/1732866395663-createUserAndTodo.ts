4cc790cf026c213f3a159ef9003296afe7aff79f
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-29 16:43:38 +0300

    New migrate

--- src/db/migrations/1732866395663-createUserAndTodo.ts
+++ src/db/migrations/1732866395663-createUserAndTodo.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createUserAndTodo1732798829332 implements MigrationInterface {
-    name = 'createUserAndTodo1732798829332'
+export class createUserAndTodo1732866395663 implements MigrationInterface {
+    name = 'createUserAndTodo1732866395663'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
@@ -9,6 +9,7 @@ export class createUserAndTodo1732798829332 implements MigrationInterface {
                 "id" SERIAL NOT NULL,
                 "text" character varying NOT NULL,
                 "isCompleted" boolean NOT NULL,
+                "userId" integer,
                 CONSTRAINT "PK_d429b7114371f6a35c5cb4776a7" PRIMARY KEY ("id")
             )
         `);
@@ -24,9 +25,16 @@ export class createUserAndTodo1732798829332 implements MigrationInterface {
                 CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
             )
         `);
+        await queryRunner.query(`
+            ALTER TABLE "todo"
+            ADD CONSTRAINT "FK_1e982e43f63a98ad9918a86035c" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            ALTER TABLE "todo" DROP CONSTRAINT "FK_1e982e43f63a98ad9918a86035c"
+        `);
         await queryRunner.query(`
             DROP TABLE "user"
         `);
