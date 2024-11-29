9d670a03695ebcdde1344e03903b3617d24faf22
Alex Koloda <xeniumsonyz@gmail.com>
2024-11-29 16:43:21 +0300

    Feat func create todo, get current todo

--- src/controllers/todo-controller.ts
+++ src/controllers/todo-controller.ts
@@ -2,19 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import todoService from "../services/todo-service";
 
 class TodoController {
-
   async createTodo(req: Request, res: Response, next: NextFunction) {
     try {
-      
+      const todo = req.body;
+      const currentUser = req.user;
+      todo.user = currentUser;
+      await todoService.createTodo(todo);
+      res.status(200).json(todo);
     } catch (err) {
       next(err);
     }
   }
 
   async getFilteredTodos(req: Request, res: Response, next: NextFunction) {
-    const { filter } = req.params;
     try {
+      const {filter} = req.params;
       const todos = await todoService.getFilteredTodo(filter);
+      res.status(200).json(todos);
     } catch (err) {
       next(err);
     }
@@ -22,7 +26,9 @@ class TodoController {
 
   async getCurrentTodo(req: Request, res: Response, next: NextFunction) {
     try {
-      
+      const {id} = req.params;
+      const currentTodo = await todoService.getCurrentTodo(Number(id));
+      res.status(200).json(currentTodo);
     } catch (err) {
       next(err);
     }
@@ -30,7 +36,6 @@ class TodoController {
 
   async deleteTodo(req: Request, res: Response, next: NextFunction) {
     try {
-      
     } catch (err) {
       next(err);
     }
@@ -38,11 +43,10 @@ class TodoController {
 
   async updateTodo(req: Request, res: Response, next: NextFunction) {
     try {
-      
     } catch (err) {
       next(err);
     }
   }
-  }
+}
 
 export default new TodoController();
