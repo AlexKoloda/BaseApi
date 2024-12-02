093bdb7ec57af600ca43e6e10866055cb811ade8
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-02 10:00:11 +0300

    Add validation for update todo

--- src/routes/todo-routes.ts
+++ src/routes/todo-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import todoController from "../controllers/todo-controller";
 import { validate } from "../validation/validate";
-import { createTodoSchema } from "../validation/todo-schemas";
+import { createTodoSchema, updateTodoSchema } from "../validation/todo-schemas";
 
 const todoRouter = Router();
 
@@ -9,6 +9,6 @@ todoRouter.post("/create", validate(createTodoSchema), todoController.createTodo
 todoRouter.get("/:id", todoController.getCurrentTodo);
 todoRouter.get("/filter/:filter", todoController.getFilteredTodos);
 todoRouter.patch("/", todoController.updateTodo);
-todoRouter.delete("/:id", todoController.deleteTodo);
+todoRouter.delete("/:id", validate(updateTodoSchema), todoController.deleteTodo);
 
 export default todoRouter;
--- src/validation/todo-schemas.ts
+++ src/validation/todo-schemas.ts
@@ -1,6 +1,11 @@
 import { string, object, boolean } from "yup";
 
 export const createTodoSchema = object({
-  text: string().required("Name must be require"),
+  text: string().required("Text must be require"),
   isCompleted: boolean().required("isCompleted must be require"),
+});
+
+export const updateTodoSchema = object({
+  text: string().min(1, "Text must be require"),
+  isCompleted: boolean(),
 });
