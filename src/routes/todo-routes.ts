7f236637788a26e54ca5a240d19f41254638a631
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-02 10:01:33 +0300

    Fix validation for update todo

--- src/routes/todo-routes.ts
+++ src/routes/todo-routes.ts
@@ -8,7 +8,7 @@ const todoRouter = Router();
 todoRouter.post("/create", validate(createTodoSchema), todoController.createTodo);
 todoRouter.get("/:id", todoController.getCurrentTodo);
 todoRouter.get("/filter/:filter", todoController.getFilteredTodos);
-todoRouter.patch("/", todoController.updateTodo);
-todoRouter.delete("/:id", validate(updateTodoSchema), todoController.deleteTodo);
+todoRouter.patch("/", validate(updateTodoSchema), todoController.updateTodo);
+todoRouter.delete("/:id", todoController.deleteTodo);
 
 export default todoRouter;
