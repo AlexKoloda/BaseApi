257c787538f3e52db371a0815ebf11384838642b
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-02 16:10:39 +0300

    Create global config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-import { TodoType, UserInterface } from './types/types';
+import { UserInterface } from './types/types';
+import { config } from 'dotenv';
+config();
 
 declare global {
   namespace Express {
@@ -7,3 +9,28 @@ declare global {
     }
   }
 }
+
+const conf = {
+  database: {
+    host: process.env.HOST,
+    userName: process.env.DB_USER_NAME,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    dbName: process.env.DB_NAME,
+  },
+
+  server: {
+    port: process.env.PORT,
+  },
+
+  token: {
+    secret: process.env.TOKEN_SECRET,
+    exp: process.env.EXP,
+  },
+
+  salt: process.env.SALT,
+};
+
+console.log(conf.database.userName);
+
+export default conf;
