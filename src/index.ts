ce8455305f06c452b05d85c2e9450fc840c8bbc2
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-06 13:57:15 +0300

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import mainRouter from './routes';
 import { CustomError } from './util/custom-errors';
 import './config';
 import conf from './config';
-
+import  * as cors from 'cors'
 
 const app = express();
 const PORT = conf.server.port || 4000;
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', mainRouter);
