870564d5bfd1a551b65a52f7b40087d81d31b12d
Alex Koloda <xeniumsonyz@gmail.com>
2024-12-17 19:02:36 +0300

    Add new migrate

--- src/db/migrations/1734433383207-CreateTodosDB.ts
+++ src/db/migrations/1734433383207-CreateTodosDB.ts
@@ -1,18 +1,9 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createUserAndTodo1732866395663 implements MigrationInterface {
-    name = 'createUserAndTodo1732866395663'
+export class CreateTodosDB1734433383207 implements MigrationInterface {
+    name = 'CreateTodosDB1734433383207'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            CREATE TABLE "todo" (
-                "id" SERIAL NOT NULL,
-                "text" character varying NOT NULL,
-                "isCompleted" boolean NOT NULL,
-                "userId" integer,
-                CONSTRAINT "PK_d429b7114371f6a35c5cb4776a7" PRIMARY KEY ("id")
-            )
-        `);
         await queryRunner.query(`
             CREATE TABLE "user" (
                 "id" SERIAL NOT NULL,
@@ -20,9 +11,17 @@ export class createUserAndTodo1732866395663 implements MigrationInterface {
                 "lastName" character varying NOT NULL,
                 "email" character varying NOT NULL,
                 "password" character varying NOT NULL,
-                "dateBirth" character varying NOT NULL,
                 CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"),
                 CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
+            )
+        `);
+        await queryRunner.query(`
+            CREATE TABLE "todo" (
+                "id" SERIAL NOT NULL,
+                "text" character varying NOT NULL,
+                "isCompleted" boolean NOT NULL,
+                "userId" integer,
+                CONSTRAINT "PK_d429b7114371f6a35c5cb4776a7" PRIMARY KEY ("id")
             )
         `);
         await queryRunner.query(`
@@ -36,10 +35,10 @@ export class createUserAndTodo1732866395663 implements MigrationInterface {
             ALTER TABLE "todo" DROP CONSTRAINT "FK_1e982e43f63a98ad9918a86035c"
         `);
         await queryRunner.query(`
-            DROP TABLE "user"
+            DROP TABLE "todo"
         `);
         await queryRunner.query(`
-            DROP TABLE "todo"
+            DROP TABLE "user"
         `);
     }
 
