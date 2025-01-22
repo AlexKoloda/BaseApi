8975b4d6be821cf5b0f4ee276739dc8a5e9d86d8
Alex Koloda <xeniumsonyz@gmail.com>
2025-01-22 11:01:05 +0300

    Feat async func for decode file

--- src/util/convert64Base.ts
+++ src/util/convert64Base.ts
@@ -1,8 +1,20 @@
-import fs = require('fs');
+import { writeFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
 
-const base64Decode = (base64Image: string, file: string) => {
-  const buff = Buffer.from(base64Image, 'base64');
-  fs.writeFileSync(file, buff);
-  console.log('User photo saved');
+
+
+const base64Decode = async (base64Image: string, file: string) => {
+
+  const data = new Uint8Array(Buffer.from(base64Image, 'base64'));
+  writeFile(file, data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+}); 
 };
+
 export default base64Decode;
+
+
+
+
+
